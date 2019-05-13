f350fa40681cdff5beaafe0b173624d283458423
patrick <princenazio2@gmail.com>
2019-05-13 18:37:37 +0100

    chore(service-worker): Register service worker

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import './scss/app.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister();
+serviceWorker.register();
